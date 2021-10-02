8c3c0b5cc078c45dbd7e7ac409916cdc45228b7b
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2021-10-02 11:54:04 +0100

    Update TOCs.js

--- knowledgebase/TOCs.js
+++ knowledgebase/TOCs.js
@@ -1,6 +1,6 @@
 // ©Cyclingbob 2021. All rights reserved. This material must not be copied and redistributed under any conditions, with or without commerical gain. Exceptions only apply where exclusive permission has been granted.
 // 2nd October 2021
-// This code generates a token to access the National Rail Open Data API (https://opendata.nationalrail.co.uk/) and then uses this to download the current Train Operating Companies. The result is a JavaScript Object.
+// This code generates a token to access the National Rail Open Data API (https://opendata.nationalrail.co.uk/) and then uses this to download the current Train Operating Companies. The result is a JavaScript Object, logged in the console.
 
 const fetch = require('node-fetch')
 const parseString = require('xml2js').parseString
