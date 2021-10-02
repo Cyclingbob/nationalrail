9857287cbe57fd2fb379de46c4b469be5d5fadac
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2021-10-02 11:16:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+// ©Cyclingbob 2021. All rights reserved. This material must not be copied and redistributed under any conditions, with or without commerical gain. Exceptions only apply where exclusive permission has been granted.
+// 2nd October 2021
+// Once cloned, add your details to access the National Rail Open Data
+
 module.exports = {
     email: 'YOUR_EMAIL', //your Open Data National Rail account email address
     password: 'YOUR_PASSWORD', //your Open Data National Rail account password
