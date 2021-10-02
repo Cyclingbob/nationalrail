c72376160e99c59f240ae8d85294bed001c3463f
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2021-10-02 12:47:05 +0100

    Update TicketTypes.js

--- knowledgebase/TicketTypes.js
+++ knowledgebase/TicketTypes.js
@@ -1,6 +1,6 @@
 // ©Cyclingbob 2021. All rights reserved. This material must not be copied and redistributed under any conditions, with or without commerical gain. Exceptions only apply where exclusive permission has been granted.
 // 2nd October 2021
-// This code generates a token to access the National Rail Open Data API (https://opendata.nationalrail.co.uk/) and then uses this to get the current Ticket Types. I've written the code so only the first station is logged to the console, but all are available. The result is a JavaScript Object.
+// This code generates a token to access the National Rail Open Data API (https://opendata.nationalrail.co.uk/) and then uses this to get the current Ticket Types. I've written the code so only the first ticket type is logged to the console, but all are available. The result is a JavaScript Object.
 
 const fetch = require('node-fetch')
 const parseString = require('xml2js').parseString
