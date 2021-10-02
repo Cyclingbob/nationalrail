ed5362446cf7b2f2f0c53b8d0c8c471b95cccea2
Cyclingbob <52578139+Cyclingbob@users.noreply.github.com>
2021-10-02 11:17:26 +0100

    Update bufferToStream.js

--- assets/bufferToStream.js
+++ assets/bufferToStream.js
@@ -1,3 +1,7 @@
+// ©Cyclingbob 2021. All rights reserved. This material must not be copied and redistributed under any conditions, with or without commerical gain. Exceptions only apply where exclusive permission has been granted.
+// 2nd October 2021
+// This code turns a response buffer into a stream. This can then be piped to a file or another function.
+
 const Readable = require('stream').Readable; 
 
 module.exports = function(buffer){
